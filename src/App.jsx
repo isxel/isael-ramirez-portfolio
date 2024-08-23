01442561536346a353e4d6bb344ded80404eb02e
Isael <iramir17@stedwards.edu>
2024-08-23 15:16:06 -0500

    Made changes to app component to include new aboutMe component and added new css files. included image of myself in assets folder. also included about me page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import LandingPage from "./components/LandingPage";
 import Projects from "./components/Projects";
+import AboutMe from "./components/AboutMe";
+import "./style/App.css";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <AboutMe />
       <Projects />
     </div>
   );
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import meImage from "../assets/me.jpg";
+import "../style/aboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <div className="about-container">
+      <div className="about-image">
+        <img src={meImage} alt="Isael Ramirez" />
+      </div>
+      <div className="about-text">
+        <h2 className="about-headings">About Me</h2>
+        <p>
+          Hello, I'm Isael Ramirez, a recent graduate with a degree in Computer
+          Science based in Austin, Texas. My passion for cars and the vibrant
+          culture surrounding them has been a significant part of my life. I
+          cherish the connections, joy, and memories that come from being
+          immersed in the automotive world.
+        </p>
+        <p>
+          Growing up, I was surrounded by Japanese cars, especially the Acura
+          Integra, which sparked my love for automobiles. Currently, I drive a
+          1990 Mazda Miata, a car I've enjoyed immensely for the better part of
+          two years. The Miata has not only been a joy to drive but also a
+          canvas for my automotive creativity and passion.
+        </p>
+        <p>
+          Photography has allowed me to capture these moments and share them
+          with others. For many, cars are more than just a mode of
+          transportation—they are a form of self-expression. I take great pride
+          in capturing the effort and creativity people pour into their
+          vehicles, preserving the essence of their individuality through my
+          camera lens.
+        </p>
+        <h2>My Tools</h2>
+        <p>
+          I use a Sony a6000 for my automotive photography. I also use Adobe
+          Lightroom and Photoshop to edit my photos, ensuring that each image is
+          a true reflection of the car and the owner's vision.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
