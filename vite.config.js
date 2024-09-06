a892ae164dcfb8f81903a488674919a2146145f4
Isael <iramir17@stedwards.edu>
2024-09-06 15:34:21 -0500

    Changes to homepage and package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "/",
   plugins: [react()],
-  base: mode === "production" ? "/my-react-portfolio/" : "/", // Use the base path only for production
-}));
+  server: {
+    historyApiFallback: true,
+  },
+});
