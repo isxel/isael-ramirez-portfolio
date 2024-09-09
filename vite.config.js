f20b77b3b706767a9b8d5d6de9a8bbb55c2df73e
Isael <iramir17@stedwards.edu>
2024-09-09 18:51:43 -0500

    Updated Vite config base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/isael-ramirez-portfolio/",
+  base: "/",
   plugins: [react()],
   server: {
     historyApiFallback: true,
