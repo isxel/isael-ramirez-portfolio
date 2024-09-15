b4de15c7a0b132e807721666810ffbb33b00b9bb
Isael <iramir17@stedwards.edu>
2024-09-15 14:23:22 -0500

    Removed unused files and updated App.jsx for old import statements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import LandingPage from "./components/LandingPage";
-import Projects from "./components/Projects";
-import AboutMe from "./components/AboutMe";
 import Proj from "./components/Proj";
 import About from "./components/About";
 import Footer from "./components/Footer";
