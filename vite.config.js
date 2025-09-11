1fa80d2ceb8ae029a76f91cceabf8f9a6eb6fbea
Isael <iramir17@stedwards.edu>
2025-09-11 15:36:32 -0500

    Corrected changes for github pages hosting.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/isael-ramirez-portfolio/",
   plugins: [react()],
   server: {
     historyApiFallback: true,
